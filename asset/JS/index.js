6a6bfd0750040727b9520592cac99ad25002f10b
AnthonyPailloux <anthonypailloux@outlook.com>
2025-07-18 16:15:37 +0200

    JavaScript

---
+++ asset/JS/index.js
@@ -0,0 +1,56 @@
+// //voici mon commentaire
+
+// const lait = 20;
+// const beurre = 35;
+// let total = 0;
+
+// total = lait + beurre;
+// console.log(total);
+
+// const chocolatine = 20;
+// const painAuchoco = 30;
+// let total = 0;
+
+// total = chocolatine + painAuchoco;
+// console.log(total);
+
+
+//exo 1
+
+const name = 'Anthony';
+console.log(name);
+alert('Anthony');
+
+//exo 2
+
+let age = 39;
+let message = " i am " + name + " et j'ai " + age;
+console.log(message);
+
+// exo 3
+
+const ageOnemore = age + 1;
+let ageMessage = "l'année prochaine j'aurais " + ageOnemore;
+console.log(ageMessage);
+
+
+// exo 4
+let toto = "toto";
+let tata = "tata";
+let invers = tata;
+tata = toto;
+toto = invers;
+console.log("dans toto:" + toto, "dans tata:" + tata);
+
+
+
+
+
+
+
+
+
+
+
+
+
